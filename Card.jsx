35b2ffb8c38fa74717fde6dceefc179cd0b05677
LuxThorley <oliverluxthorley@gmail.com>
2025-07-24 04:07:08 +0100

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,1 @@
+export function Card({ children }) { return <div className="p-4 border rounded shadow">{children}</div> }
---
+++ Input.jsx
@@ -0,0 +1,1 @@
+export function Input(props) { return <input className="w-full p-2 border rounded" {...props} /> }
